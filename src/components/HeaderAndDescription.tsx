58eadb454bf570b0b70407662aef81be7d343fc5
ing-arriola <ingeniero.arriola@gmail.com>
2022-02-15 07:00:15 -0600

    component to show a header and description in every card

---
+++ src/components/HeaderAndDescription.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Datum } from "../interfaces/search";
+
+interface Props {
+  data: Datum[];
+}
+
+const HeaderAndDescription = ({ data }: Props) => (
+  <>
+    {data.map((info) => (
+      <div key={info.nasa_id}>
+        <h3 className="text-truncate">{info.title}</h3>
+        <p>{info.description.substring(0, 200)}</p>
+      </div>
+    ))}
+  </>
+);
+
+export default HeaderAndDescription;
