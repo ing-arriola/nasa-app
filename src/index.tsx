afdf76c1b58777cfb9e42f6eb2fe73d51937f9f7
ing-arriola <ingeniero.arriola@gmail.com>
2022-02-15 06:25:35 -0600

    Added navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import App from "./App";
+import ImageLibrary from "./Screens/ImageLibrary";
+import Navbar from "./components/NavBar";
+import Header from "./components/Header";
+import ApodScreen from "./Screens/Apod/ApodScreen";
+import RoverScreen from "./Screens/RoverScreen/RoverScreen";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Navbar />
+    <Header />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/images" element={<ImageLibrary />} />
+      <Route path="/apod" element={<ApodScreen />} />
+      <Route path="/rover" element={<RoverScreen />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
