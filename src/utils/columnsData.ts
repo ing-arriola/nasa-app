1c7bbbb3ef7b3e9bec354cce6ee59f2b0d7fdef7
ing-arriola <ingeniero.arriola@gmail.com>
2022-02-15 06:27:02 -0600

    Added data to show in homeScreen

---
+++ src/utils/columnsData.ts
@@ -0,0 +1,26 @@
+export const data = [
+    {
+      id:1,
+      name: "Fa/FaRegBuilding",
+      title: "Information Library",
+      background: "#06bcee",
+      description: "Search for information in our awesome library",
+      showing:false,
+    },
+    {
+      id:2,
+      name: "Fa/FaRegStar",
+      title: "Astronomy Picture of the Day (APOD)",
+      background: "#30337d",
+      description: "Discover the universe one picture per day",
+      showing:false,
+    },
+    {
+      id:3,
+      name: "Fa/FaCameraRetro",
+      title: "Mars Rover Photos",
+      background: "#06bcee",
+      description: "Check out our astonishing photo gallery",
+      showing:false,
+    },
+  ];
